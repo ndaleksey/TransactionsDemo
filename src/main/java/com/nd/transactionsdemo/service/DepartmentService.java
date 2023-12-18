6431d719a9a7d3cba82f05ee688462f16ab85244
ND <ugtemp@mail.ru>
2023-12-18 12:28:30 +0300

    Reset propagation level

--- src/main/java/com/nd/transactionsdemo/service/DepartmentService.java
+++ src/main/java/com/nd/transactionsdemo/service/DepartmentService.java
@@ -4,16 +4,10 @@ import com.nd.transactionsdemo.dao.mapper.DepartmentMapper;
 import com.nd.transactionsdemo.model.Department;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -27,11 +21,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class DepartmentService {
 
-    private final DataSource dataSource;
     private final DepartmentMapper departmentMapper;
     private final DepartmentValidator departmentValidator;
-    private final ApplicationContext context;
-//    private final PlatformTransactionManager transactionManager;
     private final TransactionTemplate transactionTemplate;
 
     /**
@@ -45,17 +36,17 @@ public class DepartmentService {
 
     public void create(String name) {
         log.info("Try to create department");
+        var oldPropagationBehavior = transactionTemplate.getPropagationBehavior();
         try {
             departmentValidator.validateName(name);
 
             transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_NESTED);
 
-            transactionTemplate.executeWithoutResult(status -> {
-                departmentMapper.save(createDepartment(name));
-            });
-//            transactionManager.getTransaction(new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_NESTED))
+            transactionTemplate.executeWithoutResult(status -> departmentMapper.save(createDepartment(name)));
         } catch (Exception e) {
             log.error("Couldn't create department [name = {}]", name);
+        } finally {
+            transactionTemplate.setPropagationBehavior(oldPropagationBehavior);
         }
     }
 
